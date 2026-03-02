cf6ac996eaa778e5676ae067a94694f215497a3e
Divya Mypala <divyamypala04@gmail.com>
2026-03-02 15:28:17 +0000

    added Flask-project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,request,render_template
+from flask import jsonify
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['GET'])
+def get_data():
+
+  
+    with open('data.json', 'r') as file:
+
+       
+        data = json.load(file)
+
+   
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
